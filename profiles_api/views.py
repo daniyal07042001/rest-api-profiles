b9b881d0337164f96776c8099c0b7f089e9cc961
Daniyal Shahid <daniyal20010407@gmail.com>
2022-06-22 23:02:26 +0500

    Worked on Methods(post, put, patch, delete)

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test Api View"""
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a List of Api Views Features"""
 
@@ -15,3 +20,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Using Post method to show Hello with the name input"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle the updation of object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial updation of object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete the object"""
+        return Response({'method': 'DELETE'})
